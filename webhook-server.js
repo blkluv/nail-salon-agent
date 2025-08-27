36aac6686561ad2fca3d673c5b5b76335aa710ba
dropflyai <dropflyai@gmail.com>
2025-08-27 09:42:14 -0700

    Add CORS headers for web widget support

--- webhook-server.js
+++ webhook-server.js
@@ -66,6 +66,19 @@ async function lookupBusinessByPhoneId(phoneId) {
 
 app.use(express.json());
 
+// Add CORS headers for web widget
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    
+    if (req.method === 'OPTIONS') {
+        res.sendStatus(200);
+    } else {
+        next();
+    }
+});
+
 // Webhook endpoint for Vapi
 app.post('/webhook/vapi', async (req, res) => {
     try {
